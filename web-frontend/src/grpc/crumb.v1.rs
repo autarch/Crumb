b6f1dbabdb5c3286156e48f5b2ae92b747fdc8e6
Dave Rolsky <autarch@urth.org>
2022-07-23 12:59:39 -0500

    Upgrade all dependencies

--- web-frontend/src/grpc/crumb.v1.rs
+++ web-frontend/src/grpc/crumb.v1.rs
@@ -396,7 +396,7 @@ pub mod crumb_client {
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
         T::Error: Into<StdError>,
-        T::ResponseBody: Default + Body<Data = Bytes> + Send + 'static,
+        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
         <T::ResponseBody as Body>::Error: Into<StdError> + Send,
     {
         pub fn new(inner: T) -> Self {
@@ -409,6 +409,7 @@ pub mod crumb_client {
         ) -> CrumbClient<InterceptedService<T, F>>
         where
             F: tonic::service::Interceptor,
+            T::ResponseBody: Default,
             T: tonic::codegen::Service<
                 http::Request<tonic::body::BoxBody>,
                 Response = http::Response<
